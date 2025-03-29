1e3ab57c206fd836b4c559f37f9ad4723dcbec5b
Pontus Häggblom <pbhaggblom@gmail.com>
2025-03-29 23:38:20 +0100

    Klicka på bild eller text för att visa hela beskrivningen

--- js/api-script.js
+++ js/api-script.js
@@ -1,130 +1,125 @@
-
-
-document.addEventListener("DOMContentLoaded", e => {
-    fetch('https://fakestoreapi.com/products')
-    .then( e => e.json()) 
+document.addEventListener("DOMContentLoaded", (e) => {
+  fetch("https://fakestoreapi.com/products")
+    .then((e) => e.json())
     .then(visaProdukter);
 });
 
-
 function visaProdukter(produkter) {
+  let indexDiven = document.getElementById("listaProdukterna");
 
-    let indexDiven = document.getElementById("listaProdukterna");
+  for (let i = 0; i < produkter.length; i++) {
+    let produkten = produkter[i];
 
-    for (let i = 0; i < produkter.length; i++) {
-        
-        let produkten = produkter[i]; 
+    // En produkt div
+    let enskildProdukt = document.createElement("div");
+    enskildProdukt.className =
+      "card d-flex mb-4 p-3 col-12 col-md-5 col-xl-4 mx-auto border shadow-lg justify-content-between";
 
-        // En produkt div
-        let enskildProdukt = document.createElement("div");
-        enskildProdukt.className = "card d-flex mb-4 p-3 col-12 col-md-5 col-xl-4 mx-auto border shadow-lg justify-content-between";
+    // flyttade ner den till "layout-delen" - Andy
+    // // Div pris och Kanpp och bild
+    // let pris_Kanpp_Bild = document.createElement("div");
+    // pris_Kanpp_Bild.className = "d-flex align-items-center justify-content-between";
 
-        // flyttade ner den till "layout-delen" - Andy
-        // // Div pris och Kanpp och bild
-        // let pris_Kanpp_Bild = document.createElement("div");
-        // pris_Kanpp_Bild.className = "d-flex align-items-center justify-content-between";
+    // div till bild
+    let bildDiv = document.createElement("div");
+    bildDiv.className = "d-flex";
 
-        // div till bild
-        let bildDiv = document.createElement("div");
-        bildDiv.className = "d-flex"; 
+    // flyttade ner den till "layout-delen" - Andy
+    // //  Div till Pris o Knapp
+    // let pris_Knapp = document.createElement("div");
+    // pris_Knapp.className = "d-flex flex-column mx-3";
 
+    // Namnet
+    let titel = document.createElement("h3");
+    titel.textContent = produkten.title;
+    titel.classList.add("text-center", "mb-4", "px-2");
+    titel.id = "titel-" + i;
 
-        // flyttade ner den till "layout-delen" - Andy
-        // //  Div till Pris o Knapp
-        // let pris_Knapp = document.createElement("div");
-        // pris_Knapp.className = "d-flex flex-column mx-3";
+    // Beskrivning
+    let beskrivning = document.createElement("p");
+    beskrivning.textContent = produkten.description;
+    beskrivning.classList.add(
+      "text-muted",
+      "small",
+      "px-2",
+      "d-md-block",
+      "d-none",
+      "overflow-visible",
+      "beskrivning"
+    );
+    beskrivning.id = "beskrivning-" + i;
 
+    //kategori
+    let kategori = document.createElement("p");
+    kategori.textContent = produkten.category;
+    kategori.classList.add("text-decoration-underline", "px-2", "text-start");
+    kategori.id = "kategori-" + i;
 
-        // Namnet
-        let titel = document.createElement("h3");
-        titel.textContent = produkten.title; 
-        titel.classList.add("text-center", "mb-4", "px-2");
+    // Hämta och skapa bilden från "fakestoreapi"
+    let bild = document.createElement("img");
+    bild.src = produkten.image;
+    bild.classList.add("img-fluid", "mx-3", "produktbild");
+    bild.id = "bild-" + i;
 
-        // Beskrivning
-        let beskrivning = document.createElement("p");
-        beskrivning.textContent = produkten.description;
-        beskrivning.id = "beskrivning";
-        beskrivning.classList.add("text-muted", "small", "px-2", "d-md-block", "d-none", "overflow-visible");
+    // Priset
+    let pris = document.createElement("p");
+    pris.textContent = produkten.price + "$";
+    pris.classList.add("fw-bold", "mb-3");
 
-        //kategori
-        let kategori = document.createElement("p");
-        kategori.textContent = produkten.category;
-        kategori.classList.add("text-decoration-underline", "px-2", "text-start");
+    // Beställ btn
+    let beställKnapp = document.createElement("button");
+    beställKnapp.textContent = "Beställ";
+    beställKnapp.className = "btn btn-primary";
 
+    beställKnapp.addEventListener("click", (e) => {
+      location.href = "bestallning.html";
+      sessionStorage.setItem("chosenProduct", JSON.stringify(produkten));
+    });
 
-        // Hämta och skapa bilden från "fakestoreapi"
-        let bild = document.createElement("img");
-        bild.id = "produktbild";
-        bild.src = produkten.image;
-        bild.classList.add("img-fluid", "mx-3");
+    //Lägg till (gör om lite av placeringen, sparat förra dock) - Andy
 
+    //Bild & text
+    let bild_Beskrivning = document.createElement("div");
+    bild_Beskrivning.className =
+      "d-flex align-items-start justify-content-center mb-2 kort-produkt";
 
-        // Priset
-        let pris = document.createElement("p");
-        pris.textContent = produkten.price + "$";
-        pris.classList.add("fw-bold", "mb-3"); 
+    bild_Beskrivning.appendChild(bild);
+    bild_Beskrivning.appendChild(beskrivning);
 
-        // Beställ btn
-        let beställKnapp = document.createElement("button");
-        beställKnapp.textContent = "Beställ";
-        beställKnapp.className = "btn btn-primary"; 
+    bild_Beskrivning.addEventListener("click", (e) => visaBeskrivning(i));
 
-        beställKnapp.addEventListener("click", e => {
-            location.href = "bestallning.html";
-            sessionStorage.setItem('chosenProduct', JSON.stringify(produkten));
-        });
+    //Knapp & pris
+    let pris_Knapp = document.createElement("div");
+    pris_Knapp.className = "d-flex flex-column mx-3";
+    pris_Knapp.id = "order-" + i;
+    pris_Knapp.appendChild(pris);
+    pris_Knapp.appendChild(beställKnapp);
+    //pris_Knapp.style.flex = "0 0 150px"
 
+    //Raden vänster + höger
+    //let pris_Kanpp_Bild = document.createElement("div");
+    //pris_Kanpp_Bild.className = "d-flex flex-wrap align-items-center justify-content-center";
+    //pris_Kanpp_Bild.appendChild(vänsterDiv)
+    //pris_Kanpp_Bild.appendChild(pris_Knapp)
 
-        //Lägg till (gör om lite av placeringen, sparat förra dock) - Andy
+    //Hela diven
+    enskildProdukt.appendChild(kategori);
+    enskildProdukt.appendChild(titel);
+    enskildProdukt.appendChild(bild_Beskrivning);
+    enskildProdukt.appendChild(pris_Knapp);
 
-        //Bild & text
-        let vänsterDiv = document.createElement("div");
-        vänsterDiv.className = "d-flex align-items-start justify-content-center mb-2";
-        vänsterDiv.id = "kort-produkt"
-        vänsterDiv.appendChild(bild);
-        vänsterDiv.appendChild(beskrivning);
-        //vänsterDiv.style.flex = "1";
-
-        //Knapp & pris
-        let pris_Knapp = document.createElement("div");
-        pris_Knapp.className = "d-flex flex-column mx-3"
-        pris_Knapp.appendChild(pris);
-        pris_Knapp.appendChild(beställKnapp);
-        //pris_Knapp.style.flex = "0 0 150px"
-
-        //Raden vänster + höger
-        let pris_Kanpp_Bild = document.createElement("div");
-        pris_Kanpp_Bild.className = "d-flex flex-wrap align-items-center justify-content-center";
-        pris_Kanpp_Bild.appendChild(vänsterDiv)
-        //pris_Kanpp_Bild.appendChild(pris_Knapp)
-
-        //Hela diven
-        enskildProdukt.appendChild(kategori);
-        enskildProdukt.appendChild(titel);
-        enskildProdukt.appendChild(pris_Kanpp_Bild);
-        enskildProdukt.appendChild(pris_Knapp)
-
-        indexDiven.appendChild(enskildProdukt);
-
-
-
-
-        //Lägg till
-        // bildDiv.appendChild(bild);
-        //
-        // pris_Knapp.appendChild(pris);
-        // pris_Knapp.appendChild(beställKnapp);
-        //
-        // pris_Kanpp_Bild.appendChild(bildDiv);
-        // pris_Kanpp_Bild.appendChild(pris_Knapp);
-        //
-        // enskildProdukt.appendChild(kategori)
-        // enskildProdukt.appendChild(titel);
-        // enskildProdukt.appendChild(beskrivning)
-        // enskildProdukt.appendChild(pris_Kanpp_Bild);
-        //
-        // indexDiven.appendChild(enskildProdukt);
-    }
+    indexDiven.appendChild(enskildProdukt);
+  }
 }
 
-
+function visaBeskrivning(i) {
+  if (document.getElementById("bild-" + i).style.display !== "none") {
+    document.getElementById("bild-" + i).style.display = "none";
+    document.getElementById("beskrivning-" + i).classList.remove("beskrivning");
+    document.getElementById("beskrivning-" + i).classList.remove("d-none");
+  } else {
+    document.getElementById("bild-" + i).style.display = "block";
+    document.getElementById("beskrivning-" + i).classList.add("beskrivning");
+    document.getElementById("beskrivning-" + i).classList.add("d-none");    
+  }
+}
